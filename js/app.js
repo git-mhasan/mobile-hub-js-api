39822e458cd40a15572cb37027cd7bf76c7ad9b8
git-mhasan <mahadi2020dev@gmail.com>
2022-03-01 09:10:55 +0600

    js function outline

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log('hello.')
+
+document.getElementById('search-btn').addEventListener('click', () => {
+    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+        .then(response => response.json())
+        .then(data => console.log(data.data));
+});
+
+const searchData = data => {
+
+}
+
+// function for populating search result
+
+const populateProductCard = (imageString, productName, brandName) => {
+
+}
