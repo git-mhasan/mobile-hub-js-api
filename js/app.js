1a42e74fb66b2a1d0ca8aac5de7da1db91e493d3
git-mhasan <mahadi2020dev@gmail.com>
2022-03-01 09:49:50 +0600

    setting search api

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
-console.log('hello.')
+// function for getting dom element by id Name
+const getDomElement = idName => document.getElementById(idName);
 
+const inputText = getDomElement('input-txt');
 document.getElementById('search-btn').addEventListener('click', () => {
-    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-        .then(response => response.json())
-        .then(data => console.log(data.data));
+    if (!inputText.value) {
+        // console.log('boo');
+    } else if (inputText.value.trim().length === 0) {
+        // console.log('all space');
+        inputText.value = '';
+    } else {
+        const searchUrl = `https://openapi.programming-hero.com/api/phones?search=${inputText.value.trim().toLowerCase()}
+        `;
+
+        searchProduct(searchUrl);
+        // inputText.value = '';
+
+    }
+
 });
 
-const searchData = data => {
+const searchProduct = data => {
 
+    console.log(data);
+    // console.log(getDomElement('result-section'));
 }
 
 // function for populating search result
-
-const populateProductCard = (imageString, productName, brandName) => {
+const populateProductCard = (imageString, productName, brandName, productDetails) => {
 
 }
+
